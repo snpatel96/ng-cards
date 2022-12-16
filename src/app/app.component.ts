74cf352317040b3440bc8667fc4740e849e6910d
snehal patel <snpatel.dev@gmail.com>
2022-12-16 18:46:36 -0500

    #2 Input binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  posts = [
+    {
+      title: 'Neat Tree',
+      username: 'nature',
+      imageUrl: 'assets/tree.jpeg',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Snowy Mountain',
+      username: 'mountainlover',
+      imageUrl: 'assets/mountain.jpeg',
+      content: 'Here is a picture of a snowy mountain',
+    },
+    {
+      title: 'Mountain Biking',
+      username: 'biking1222',
+      imageUrl: 'assets/biking.jpeg',
+      content: 'I did some biking today',
+    },
+  ];
+}
